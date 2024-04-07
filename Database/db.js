a7d5ddfecf0664fbed30629388a68d7cfff9d1c2
Aviraj <panditaviraj99@gmail.com>
2024-04-07 13:10:45 +0530

    adding new files

---
+++ Database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+       await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+mongoose.Promise= global.Promise;
+
+module.exports = connectDB;
